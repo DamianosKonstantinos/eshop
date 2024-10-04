64f74ea09d86fb958a88ee1b9553de8761cd3d4c
KostasDamianos <kodamianos@deloitte.gr>
2024-10-04 15:40:29 +0300

    first commit

---
+++ eshop-project/src/test/java/com/reskill/eshop_project/EshopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reskill.eshop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
